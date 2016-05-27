a14c679d29107f795478aea4aa314850fe8390ed
caofangsheng93 <fangsheng93@163.com>
2016-05-27 17:12:50 +0800

    下拉框联动

---
+++ CascadingDropdownListMVC/Models/AddressRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CascadingDropdownListMVC.Models
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private AddressDataContext _dataContext;
+
+        /// <summary>
+        /// 手动写无参的构造函数
+        /// </summary>
+        public AddressRepository()
+        {
+            _dataContext = new AddressDataContext();
+        }
+        public IList<Country> GetAllCountries()
+        {
+            var query = from countries in _dataContext.Country select countries;
+            var content = query.ToList();
+            return content;
+            //throw new NotImplementedException();
+        }
+
+        public IList<State> GetAllStatesByCountryID(int countryID)
+        {
+            var query = from states in _dataContext.State where states.CountryId == countryID select states;
+            var content = query.ToList();
+            return content;
+
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ CascadingDropdownListMVC/Models/IAddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CascadingDropdownListMVC.Models
+{
+   public interface IAddressRepository
+    {
+        IList<Country> GetAllCountries();
+        IList<State> GetAllStatesByCountryID(int countryID);
+    }
+}
